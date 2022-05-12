798d56eb4f29c6d1358a4e67cd881e68035e80a6
Wonhyuk Yang <vvghjk1234@gmail.com>
2022-05-12 03:33:21 +0000

    Fix print format of dmesg
    Signed-off-by: Wonhyuk Yang <vvghjk1234@gmail.com>

--- lock_module/parse_dmesg.py
+++ lock_module/parse_dmesg.py
@@ -1,11 +1,13 @@
 import subprocess
 
-cc_lock = {"name":"cc_lock", "nr":0, "time":list()}
+cc_lock = {"name":"cc_lock  ", "nr":0, "time":list()}
 spin_lock = {"name":"spin_lock", "nr":0, "time":list()}
 
 def print_stat(target):
+    avg = sum(target["time"]) / target["nr"]
     print("[{0}] average time: {1}, nr: {2}".format(target["name"], 
-            sum(target["time"]) / target["nr"], target["nr"]))
+            avg, target["nr"]))
+    return avg
 
 if __name__ == "__main__":
     lines = subprocess.check_output(["sudo", "dmesg"]).decode()
@@ -23,6 +25,8 @@ if __name__ == "__main__":
         target["nr"] += 1
         target["time"].append(int(line[line.rfind("[")+1:-1]))
 
-    print_stat(cc_lock)
-    print_stat(spin_lock)
+    cc_avg = print_stat(cc_lock)
+    spin_avg = print_stat(spin_lock)
+
+    print("improvment: {0:.3f}%".format((spin_avg-cc_avg)/spin_avg*100))
 
