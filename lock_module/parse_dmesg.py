11d832fa7ebaf96c9f3d2d0cd27f4fa4967177b5
Wonhyuk Yang <vvghjk1234@gmail.com>
2022-05-27 02:21:30 +0000

    Add variance

--- lock_module/parse_dmesg.py
+++ lock_module/parse_dmesg.py
@@ -1,14 +1,17 @@
 import subprocess
 import psutil
+import math
 
-clk = 1 / psutil.cpu_freq().current
+clk = 1 / (psutil.cpu_freq().current / 1000)
 cc_lock = {"name":"cc_lock  ", "nr":0, "time":list()}
 spin_lock = {"name":"spin_lock", "nr":0, "time":list()}
 
 def print_stat(target):
-    avg = sum(target["time"]) / target["nr"]
-    print("[{0}] average clock: {1:15.5f}, nr: {2}".format(target["name"],
-            avg/clk, target["nr"]))
+    avg = sum(target["time"])*1000 / target["nr"]
+    print(target["time"])
+    var = sum([(avg-i)**2 for i in target["time"]]) / len(target["time"])
+    print("[{0}] average clock: {1:15.5f} co: {2:15.5f}, nr: {3}".format(target["name"],
+            avg, math.sqrt(var), target["nr"]))
     return avg
 
 if __name__ == "__main__":
@@ -25,7 +28,7 @@ if __name__ == "__main__":
             continue
 
         target["nr"] += 1000
-        target["time"].append(int(line[line.rfind("[")+1:-1]))
+        target["time"].append(int(line[line.rfind("[")+1:-1])/(clk*1000))
 
     cc_avg = print_stat(cc_lock)
     spin_avg = print_stat(spin_lock)
